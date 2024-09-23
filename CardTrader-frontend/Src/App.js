9a1b349d41df21810a5ae9c42b584dd963e86ac6
foxxxino <98033613+ArtDadaNft@users.noreply.github.com>
2024-09-23 16:23:45 +0200

    Update App.js

---
+++ CardTrader-frontend/Src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import SellCard from './components/SellCard';
